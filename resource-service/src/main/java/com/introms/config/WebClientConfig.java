8decee31a6f6424f227db099b5b186bcf1afea9f
Ebubekir Temizkan <ebubekir_temizkan@epam.com>
2025-09-09 23:23:38 +0300

    add service-discovery

--- resource-service/src/main/java/com/introms/config/WebClientConfig.java
+++ resource-service/src/main/java/com/introms/config/WebClientConfig.java
@@ -5,9 +5,12 @@ import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
@@ -24,8 +27,15 @@ public class WebClientConfig {
 
     private final SongServiceConfigProperties configProperties;
 
+    @Bean("lbWebClient")
+    @LoadBalanced
+    @Primary
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
     @Bean
-    public WebClient songServiceClient(WebClient.Builder builder) {
+    public WebClient songServiceClient(@Qualifier("lbWebClient") WebClient.Builder builder) {
         var httpClient = HttpClient.create()
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, (int) configProperties.getTimeout().getConnect().toMillis())
                 .doOnConnected(connection ->
